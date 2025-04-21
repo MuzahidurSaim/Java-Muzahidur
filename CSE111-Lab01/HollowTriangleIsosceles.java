15b0aa2b45197812bafe305275e1d1d40078aaf6
Muzahidur.Saim <149346866+MuzahidurSaim@users.noreply.github.com>
2025-04-21 09:01:15 +0600

    Add files via upload

---
+++ CSE111-Lab01/HollowTriangleIsosceles.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class HollowTriangleIsosceles {
+    
+    public static void main(String[] args) {
+        
+        // take line numbers
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of Lines: ");
+        int lines = input.nextInt();
+        
+        // compute the pattern
+        for(int i = 1; i <= lines; ++i) {
+            for(int j = 1; j <= 2*lines - 1; ++j) {
+                if(i == lines)
+                    System.out.print(j);
+                else if(j == lines - i + 1)
+                    System.out.print(1);
+                else if(j == lines + i - 1)
+                    System.out.print(2*i - 1);
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
